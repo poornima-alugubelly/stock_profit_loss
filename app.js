e2a8bbb2f36c424a4becd68c7f3d3c4e9e501f48
poornima-alugubelly <reddypoornima683@gmail.com>
2021-08-27 15:02:20 +0530

    refactor: added image using innerHTML instead of div

--- app.js
+++ app.js
@@ -3,59 +3,51 @@ var stockQuantity = document.querySelector("#stock-quanity");
 var currentPrice = document.querySelector("#current-price");
 var form = document.querySelector("#form");
 var output = document.querySelector("#output");
-var image = document.querySelector(".image");
 
-function checkHandler(){
-  
-  image.style.display="none";  //added for edge case where user doesn't enter all values after he has entered all values and seen the result once
-  document.body.classList.remove("sadTheme"); //added because else the sad theme will persist
-  var purchasedAt = Number(purchasePrice.value);
-  var quantity = Number(stockQuantity.value);
-  var currPrice = Number(currentPrice.value);
- 
-  console.log(purchasedAt);
-  if( !(purchasePrice.value==="") && !(stockQuantity.value==="") && !(currentPrice.value==="")){
-    if(purchasedAt<0 || quantity <0 || currPrice<0){
-      output.innerHTML = "Please enter a valid value";
-      return 0;
-    }
-    calculateStock(purchasedAt,quantity,currPrice);
-  }
+function checkHandler() {
+	document.body.classList.remove("sadTheme"); //added because else the sad theme will persist
+	var purchasedAt = Number(purchasePrice.value);
+	var quantity = Number(stockQuantity.value);
+	var currPrice = Number(currentPrice.value);
 
-  else{
-    output.innerHTML = "Please enter all the values";
-  }
-
+	if (
+		!(purchasePrice.value === "") &&
+		!(stockQuantity.value === "") &&
+		!(currentPrice.value === "")
+	) {
+		if (purchasedAt < 0 || quantity < 0 || currPrice < 0) {
+			output.innerText = "Please enter a valid value";
+			return 0;
+		}
+		calculateStock(purchasedAt, quantity, currPrice);
+	} else {
+		output.innerText = "Please enter all the values";
+	}
 }
 
-function calculateStock(purchasedAt,quantity,currPrice) {
-  
-  var difference = (currPrice - purchasedAt) * quantity;
-  var earnings = ((currPrice - purchasedAt) / purchasedAt) * 100;
-  earnings = Math.abs(earnings).toFixed(2);
-  image.style.display="block"; //to undo style="none" 
-  image.style.opacity ="1.0";  // to undo opacity=0.0 
- 
+function calculateStock(purchasedAt, quantity, currPrice) {
+	var difference = (currPrice - purchasedAt) * quantity;
+	var earnings = ((currPrice - purchasedAt) / purchasedAt) * 100;
+	earnings = Math.abs(earnings).toFixed(2);
 
-  if (difference > 0) {
-    output.innerHTML = `You gained ${earnings}% Your total profit 
-                        is Rs.${difference}` + "🎉";
-    image.src="./img/krabsmoney.gif";
-    
+	if (difference > 0) {
+		output.innerHTML = `<div class="output-txt"> You gained ${earnings}% Your total profit 
+                        is Rs.${difference} 🎉 </div>
+                        <img id="image" src="./img/krabsmoney.gif">`;
+	}
+	if (difference < 0) {
+		output.innerHTML = `<div class="output-txt"> You lost ${earnings}% Your total loss 
+    is Rs.${Math.abs(difference)} 😔 </div>
+    <img id="image" src="./img/spongbobsad.gif">`;
 
-  }
-  if (difference < 0) {
-    output.innerHTML = `You lost ${earnings}% Your total loss 
-                        is Rs.${Math.abs(difference)}` + "😔";
-        if(earnings>50){
-          document.body.classList.add("sadTheme");
-        }
-    image.src ="./img/spongbobsad.gif"
-  }
+		if (earnings > 50) {
+			document.body.classList.add("sadTheme");
+		}
+	}
 
-  if(difference===0) {
-    output.innerHTML = "There was no loss or profit!😶"
-    image.src="./img/nothing.gif";
-  }
+	if (difference === 0) {
+		output.innerHTML = `<div class="output-txt"> There was no loss or profit!😶</div>
+    <img id="image" src="./img/nothing.gif">`;
+	}
 }
 form.addEventListener("submit", checkHandler);
